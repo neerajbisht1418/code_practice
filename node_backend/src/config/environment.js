9488186299b12fc0747126b8ab0f640332737dcf
neeraj <neerajsinghb.brainerhub@gmail.com>
2024-10-20 12:53:49 +0530

    update backend env variable

--- node_backend/src/config/environment.js
+++ node_backend/src/config/environment.js
@@ -9,5 +9,6 @@ module.exports = {
   MONGODB_URI: process.env.MONGODB_URI,
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRE: process.env.JWT_EXPIRE || '1h',
-  APP_VERSION : process.env.APP_VERSION || 'v1'
+  APP_VERSION : process.env.APP_VERSION || 'v1',
+  FRONTEND_URL : process.env.NODE_ENV === 'development' ? process.env.LOCAL_FRONTEND_URL:process.env.LIVE_FRONTEND_URL
 };
